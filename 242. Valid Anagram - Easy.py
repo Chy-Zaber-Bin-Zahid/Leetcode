49f59623bd858f71fb4de7926b9a2875633f4b4a
Chy-Zaber-Bin-Zahid <czaber47@gamil.com>
2024-06-30 22:36:30 +0600

    solved "valid anagram - easy"

--- 242. Valid Anagram - Easy.py
+++ 242. Valid Anagram - Easy.py
@@ -1,1 +1,27 @@
-print("heelo world")
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        ### time complexity )=O(n^2) ###
+        # if len(s) == len(t):
+        #     for i in range(len(s)):
+        #         if s[i] in t:
+        #             if s.count(s[i]) != t.count(s[i]):
+        #                 return False 
+        #         else:
+        #             return False
+        #     return True
+        # else:
+        #     return False
+        
+        ### time complexity O(n) ###
+        if len(s) != len(t):
+            return False
+        count_s = {}
+        count_t = {}        
+        for char in s:
+            count_s[char] = count_s.get(char, 0) + 1
+        for char in t:
+            count_t[char] = count_t.get(char, 0) + 1
+        return count_s == count_t
+  
+sol = Solution()
+print(sol.isAnagram("anagram", "nagaram"))
